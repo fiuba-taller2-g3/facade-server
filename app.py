f2d3a658e8a0f163bd243b168ee5a5f9b220999b
Josefina Montenegro <mariajosefina.mont@gmail.com>
2020-10-24 21:45:24 -0300

    fix

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask, request
-
-from users_service import login
+import requests
+from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
 
+def login(email, password):
+    response = requests.post('https://users-server-develop.herokuapp.com/users/login',
+                             data={"username": email, "password": password})
+
+    print("response:", response.content)
+
+    if response.status_code == 200:
+        return jsonify({"msg": "Usuario logueado exitosamente"})
+    elif response.status_code == 404:
+        return make_response(jsonify({"msg": "Usuario y/o contraseña invalidos"}), 404)
+    else:
+        return response.content
 
 @app.route('/hello/<name>')
 def hello_name(name):
