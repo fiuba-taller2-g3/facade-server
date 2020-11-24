dcc8846f6927c951512a87d09969b86b515c8c9f
Patricio Avigliano <patricioavigliano@gmail.com>
2020-11-24 02:57:17 +0100

    posts firs endpoints

--- posts_service.py
+++ posts_service.py
@@ -1,4 +1,33 @@
 import os
 import requests
 import json
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
+
+try:
+    posts_base_url = os.environ['POSTS_URL']
+except KeyError:
+    posts_base_url = 'https://posts-server-develop.herokuapp.com/'
+
+def new(req):
+    response = requests.post(posts_base_url + request.full_path, json=request.json)
+    return response.content
+
+def visualize(request):
+    response = requests.get(posts_base_url + request.full_path)
+    return response.content
+
+def edit(request):
+    response = requests.patch(posts_base_url + request.full_path, json=request.json)
+    return response.content
+
+def delete(request):
+    response = requests.delete(posts_base_url + request.full_path)
+    return response.content
+
+def visualize_from_user(request):
+    response = requests.get(posts_base_url + request.full_path)
+    return response.content
+
+def reset(request):
+    response = requests.delete(posts_base_url + request.full_path)
+    return response.content
